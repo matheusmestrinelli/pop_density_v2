e2a465cb9d1a94db35847b3d033065b5eafb4387
matheusmestrinelli <matheusmestrinelli@gmail.com>
2025-12-03 23:14:19 -0300

    Define default parameters in parameters.py
    Added configurable default parameters for various settings.

--- src/parameters.py
+++ src/parameters.py
@@ -1,1 +1,29 @@
+# Parâmetros configuráveis e padrões
+DEFAULT_PARAMS = {
+    "buffers": {
+        "fg_size": 50.0,
+        "cv_size": 100.0, 
+        "grb_size": 200.0,
+        "adj_size": 500.0
+    },
+    "ibge": {
+        "urls": [
+            "https://geoftp.ibge.gov.br/.../grade_id26.zip"
+        ],
+        "cache_dir": "data/ibge_cache/"
+    },
+    "map": {
+        "zoom": 13,
+        "cmap": "YlOrBr",
+        "basemap": "OpenStreetMap.Mapnik"
+    },
+    "report": {
+        "include_plots": True,
+        "format": "pdf"
+    }
+}
 
+def load_params(config_path="data/config.yaml"):
+    """Carrega parâmetros de YAML ou usa defaults."""
+    # Implementação flexível para expansão
+    pass
